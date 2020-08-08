7c6d4ab565c8891104363523bb48d516e98e5846
Nela Villa <b.villacastro@gmail.com>
2020-08-08 02:49:51 -0400

    prueba1

---
+++ src/components/App/barraNav.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="barraNav">
+    <!---Barra navegación---->
+<b-navbar toggleable="lg" type="dark">
+    <b-navbar-brand href="/">RM Norte</b-navbar-brand>
+ 
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto" id="nav__der">
+                <b-nav-item href="#bienvenida" v-show="!usuarioLogeado" right>Bienvenidos todas y todos</b-nav-item>
+                <b-nav-item v-for="(user,index) in user_login" :key="index" href="#bienvenida" v-show="usuarioLogeado" right>Hola {{user_login[0]}}</b-nav-item>
+                <logOut v-show="usuarioLogeado"/>
+              
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import logOut from '@/components/App/logOut.vue'
+    export default {
+        name:"barraNav",
+    
+    components:{
+      logOut
+    },
+    computed: {
+      ...mapState(['user_login', 'usuarioLogeado'])
+    },
+    }
+</script>
+
+<style lang="scss" scoped>
+$color-navbar: #17B277;
+#nav__der{
+  align-items: center;
+}
+.navbar {
+background-color:$color-navbar;
+}
+
+</style>
---
+++ src/components/App/logOut.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="botonLogOut">
+ 
+ <button id="boton__lO" @click="logOut">
+     Cerrar Sesión
+ </button>
+    </div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+    export default {
+        name:"botonLogOut",
+        methods: {
+            logOut(){
+      this.$store.dispatch('salirUsuario')
+              this.$router.replace({ name: "Home" });
+
+           }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+#boton__lO{
+    color: #fdf5ec;
+    background-color: transparent;
+    border: 1px solid #fdf5ec;
+}
+#boton__lO:hover{
+    color: #5bd8ec;
+    background-color: transparent;
+    border: 1px solid #5bd8ec;
+    border-radius: 15px;
+    transition: all 0.3s ease 0s;
+}
+#boton__lO:focus{
+    color: #5bd8ec;
+    background-color: transparent;
+    border: 1px solid #5bd8ec;
+    border-radius: 15px;
+     transition: all 0.3s ease 0s;
+}
+#boton__lO:active{
+    color: #5bd8ec;
+    background-color: transparent;
+    border: 1px solid #5bd8ec;
+    border-radius: 15px;
+     transition: all 0.3s ease 0s;
+}
+</style>
